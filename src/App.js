7db1b22e525bfb5cd260f0fa4bd21524e866c058
Cassidy Williams <cassidoo@gmail.com>
2020-01-21 20:26:35 -0800

    Initial accordion working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import { FaMountain, FaSkiing, FaWineGlassAlt } from "react-icons/fa";
+import "./App.scss";
+
+function Accordion({ data }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <div data-accordion>
+      {data.map((tab, index) => {
+        const isActive = index === activeIndex;
+        return (
+          <Fragment key={index}>
+            <div
+              data-panel-title
+              className={isActive ? "expanded" : ""}
+              onClick={() => setActiveIndex(index)}
+            >
+              <span>{tab.label}</span>
+              <span>{tab.icon}</span>
+            </div>
+            <div
+              data-panel-content
+              className={isActive ? "expanded" : ""}
+              onClick={() => setActiveIndex(index)}
+            >
+              {tab.content}
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
 
 function App() {
+  const data = [
+    {
+      label: "Dornbirn",
+      icon: <FaMountain />,
+      content:
+        "Dornbirn is the largest city in Vorarlberg and the tenth largest in Austria. It is an important commercial and shopping centre."
+    },
+    {
+      label: "Lech",
+      icon: <FaSkiing />,
+      content:
+        "Lech am Arlberg is a mountain village and an exclusive ski resort in the Bludenz district in the Austrian state of Vorarlberg on the banks of the river Lech."
+    },
+    {
+      label: "Madrid",
+      icon: <FaWineGlassAlt />,
+      content: "Madrid is the capital and most populous city of Spain."
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion data={data} />
     </div>
   );
 }
